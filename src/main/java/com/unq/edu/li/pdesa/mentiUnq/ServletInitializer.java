8672adba48763248be298da8012b79aec84e4b0f
Pais Mariano <32342901+PaisMariano@users.noreply.github.com>
2022-08-30 11:46:59 -0300

    Add files via upload

---
+++ src/main/java/com/unq/edu/li/pdesa/mentiUnq/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.unq.edu.li.pdesa.mentiUnq;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MentiUnqApplication.class);
+	}
+
+}
---
+++ src/test/java/com/unq/edu/li/pdesa/mentiUnq/MentiUnqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unq.edu.li.pdesa.mentiUnq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentiUnqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
