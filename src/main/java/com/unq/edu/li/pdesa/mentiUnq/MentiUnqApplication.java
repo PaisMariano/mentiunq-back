e2381e5b591a9d374941e6467d26cd9a2dc48a41
PaisMariano <mariano_a_p@hotmail.com>
2022-08-31 22:55:47 -0300

    primer commit hola mundo

--- src/main/java/com/unq/edu/li/pdesa/mentiUnq/MentiUnqApplication.java
+++ src/main/java/com/unq/edu/li/pdesa/mentiUnq/MentiUnqApplication.java
@@ -2,12 +2,14 @@ package com.unq.edu.li.pdesa.mentiUnq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.origin.SystemEnvironmentOrigin;
 
 @SpringBootApplication
 public class MentiUnqApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MentiUnqApplication.class, args);
+		System.out.println("Hola mundo");
 	}
 
 }
