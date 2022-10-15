b50c481fe5950157129853bb7152640b763d82db
PaisMariano <mariano_a_p@hotmail.com>
2022-10-15 11:09:03 -0300

    added new endpoint to create client answers

--- src/main/java/com/unq/edu/li/pdesa/mentiUnq/repositories/AnswerRepository.java
+++ src/main/java/com/unq/edu/li/pdesa/mentiUnq/repositories/AnswerRepository.java
@@ -1,7 +1,12 @@
 package com.unq.edu.li.pdesa.mentiUnq.repositories;
 
 import com.unq.edu.li.pdesa.mentiUnq.models.MentiOption;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AnswerRepository extends CrudRepository<MentiOption, Long> {
+    //@Query("SELECT mo, sum(mo.score) as score FROM MentiOption mo WHERE mo.question.id = :questionId GROUP BY mo.name")
+    List<MentiOption> findAllByQuestionId(Long questionId);
 }
