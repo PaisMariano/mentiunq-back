b6a065a2e8061072f9ed541eda95a5a381946055
Pais Mariano <32342901+PaisMariano@users.noreply.github.com>
2022-10-19 19:43:33 -0300

    Update AnswerRepository.java

--- src/main/java/com/unq/edu/li/pdesa/mentiUnq/repositories/AnswerRepository.java
+++ src/main/java/com/unq/edu/li/pdesa/mentiUnq/repositories/AnswerRepository.java
@@ -7,6 +7,20 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface AnswerRepository extends CrudRepository<MentiOption, Long> {
-    //@Query("SELECT mo, sum(mo.score) as score FROM MentiOption mo WHERE mo.question.id = :questionId GROUP BY mo.name")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     List<MentiOption> findAllByQuestionId(Long questionId);
 }
